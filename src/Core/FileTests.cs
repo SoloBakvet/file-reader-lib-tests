2bcd26844361c0d4d3dab7f602083db5cd752174
Stijn <Stijn@WORKSTATION>
2024-01-11 14:00:44 +0100

    Add project files.

---
+++ src/Core/FileTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileReaderLib.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace FileReaderLib.Core.Tests
+{
+    [TestClass()]
+    public class FileTests
+    {
+        [TestMethod()]
+        public void LoadContentTest1()
+        {
+            byte[] fileContent = [5, 8, 9, 10];
+            string fileName = "FileTest1.txt";
+
+            // Write to file.
+            System.IO.File.WriteAllBytes(fileName, fileContent);
+
+            // Read from file.
+            File file = new (fileName);
+            byte[] readContent = file.LoadContent();
+             
+            // Assert original and read values are the same. 
+            CollectionAssert.AreEqual(readContent, fileContent);
+        }
+
+        [TestMethod()]
+        public void LoadContentTest2()
+        {
+            byte[] fileContent = [1, 2, 3, 4, 240, 60, 80, 9,];
+            string fileName = "FileTest2.txt";
+
+            // Write to file.
+            System.IO.File.WriteAllBytes(fileName, fileContent);
+
+            // Read from file.
+            File file = new (fileName);
+            byte[] readContent = file.LoadContent();
+
+            // Assert original and read values are the same. 
+            CollectionAssert.AreEqual(readContent, fileContent);
+        }
+    }
+}
---
+++ src/Core/TextFileTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileReaderLib.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReaderLib.Core.Tests
+{
+    [TestClass()]
+    public class TextFileTests
+    {
+        [TestMethod()]
+        public void LoadContentTest1()
+        {
+            string fileContent = "Hello world, this is a test.";
+            string fileName = "TextFileTest1.txt";
+
+            // Write to file.
+            System.IO.File.WriteAllText(fileName, fileContent);
+
+            // Read from file.
+            TextFile file = new(fileName);
+            string readContent = file.LoadContent();
+
+            // Assert original and read value are the same. 
+            Assert.AreEqual(readContent, fileContent);
+        }
+
+        [TestMethod()]
+        public void LoadContentTest2()
+        {
+            string fileContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
+                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            string fileName = "TextFileTest1.txt";
+
+            // Write to file.
+            System.IO.File.WriteAllText(fileName, fileContent);
+
+            // Read from file.
+            TextFile file = new(fileName);
+            string readContent = file.LoadContent();
+
+            // Assert original and read value are the same. 
+            Assert.AreEqual(readContent, fileContent);
+        }
+    }
+}
